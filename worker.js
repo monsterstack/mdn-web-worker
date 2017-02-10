6a9bcc45d140602c866d51438b1dd8b6df39bf0d
zacharyrote <zachary.rote@gmail.com>
2017-02-10 16:57:04 -0500

    initial commit

---
+++ worker.js
@@ -0,0 +1,44 @@
+'use strict';
+const config = require('config');
+const Worker = require('core-worker').Worker;
+
+
+const main = () => {
+  console.log('Starting WEb Worker');
+  let announcement = require('./announcement.json');
+
+  let worker = new Worker("SmsWorker", announcement, {
+    queue: "sms",
+    redis: {
+      host: config.redis.host,
+      port: config.redis.port
+    },
+    discoveryHost: config.discovery.host,
+    discoveryPort: config.discovery.port,
+    typeQuery: ["TenantService"]
+  });
+
+  worker.init().then(() => {
+    // Announce
+    worker.announce();
+
+    worker.listen().then(() => {
+      worker.on('message', (workLoad) => {
+        console.log(workLoad);
+      });
+
+      worker.on('error', (err) => {
+        console.log(err);
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+
+if(require.main === module) {
+  main();
+}
